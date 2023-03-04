298d0524b664b8c7010a69d4924e3cd02cb047f9
Melaine Euzenat <melaine.euzenat@gmail.com>
2023-03-04 11:03:33 +0100

    Clean initial template part 1

--- src/renderer/screens/MainScreen.vue
+++ src/renderer/screens/MainScreen.vue
@@ -1,15 +1,7 @@
 <script setup lang="tsx">
-import { useTheme } from 'vuetify'
 import { openExternal } from '@/renderer/utils'
-import { useCounterStore } from '@/renderer/stores/counter'
-import { useSettingsStore } from '@/renderer/stores/settings'
-import { storeToRefs } from 'pinia'
 import { onMounted, ref } from 'vue'
 
-const { counterIncrease } = useCounterStore()
-const { counter } = storeToRefs(useCounterStore())
-const settingsStore = useSettingsStore()
-const theme = useTheme()
 const appVersion = ref('Unknown')
 
 onMounted((): void => {
@@ -20,22 +12,10 @@ onMounted((): void => {
   window.mainApi.send('msgRequestGetVersion')
 })
 
-const handleChangeTheme = (): void => {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-}
-
 const handleGitHub = async (): Promise<void> => {
   await openExternal('https://github.com/Prikers/piclab')
 }
 
-const handleCountIncrease = (): void => {
-  counterIncrease(1)
-}
-
-const SetNewSettings = async (): Promise<void> => {
-  settingsStore.updateSettings('theme', 'dark')
-  settingsStore.updateSettings('locale', 'fr')
-}
 </script>
 
 <template>
@@ -51,35 +31,12 @@ const SetNewSettings = async (): Promise<void> => {
           >App Version: <strong>{{ appVersion }}</strong></p
         >
         <v-row class="my-4">
-          <v-col cols="3">
-            <v-btn icon color="primary" @click="handleChangeTheme">
-              <v-icon icon="mdi-brightness-6" />
-              <v-tooltip activator="parent" location="bottom">
-                {{ $t('menu.change-theme') }}
-              </v-tooltip>
-            </v-btn>
-          </v-col>
-          <v-col cols="3">
-            <v-badge color="blue" :content="counter">
-              <v-btn icon color="primary" @click="handleCountIncrease">
-                <v-icon icon="mdi-plus-circle" />
-                <v-tooltip activator="parent" location="bottom">
-                  {{ $t('menu.increase-count') }}
-                </v-tooltip>
-              </v-btn>
-            </v-badge>
-          </v-col>
-          <v-col cols="3">
+          <v-col cols="12">
             <v-btn icon color="primary" @click="handleGitHub">
               <v-icon icon="mdi-github" />
               <v-tooltip activator="parent" location="bottom">
                 {{ $t('menu.github') }}
               </v-tooltip>
-            </v-btn>
-          </v-col>
-          <v-col cols="3">
-            <v-btn icon color="primary" @click="SetNewSettings">
-              <v-icon icon="mdi-cog" />
             </v-btn>
           </v-col>
         </v-row>
